dc3a8fa651e2e4f5c8d36ea8e8826ebb8142f275
goemktg <samktg52@gmail.com>
2024-02-14 15:25:04 +0900

    feat: 외부 라이브러리 관련 업데이트

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Require the necessary discord.js classes
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Events, GatewayIntentBits, ButtonBuilder, ButtonStyle, ActionRowBuilder, Collection, ActivityType } = require('discord.js');
+const { Client, Events, GatewayIntentBits, ButtonBuilder, ButtonStyle, ActionRowBuilder, ActivityType } = require('discord.js');
 
 // load from .env if exists
 try {
@@ -28,26 +28,14 @@ for (const file of eventFiles) {
 	}
 }
 
-// 명령어 파일 init
-client.commands = new Collection();
+// 봇에 명령어 등록
+(async () => {
+	const handleCommands = require('./library/commands-handler.js');
+	const commends = await handleCommands('init');
+	//console.log('recived commends:', commends);
+	client.commands = commends;
+})();
 
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
-
-for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
-}
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
