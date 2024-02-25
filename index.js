f32dbb91973c06b39474a5e42143c42d4060013d
goemktg <samktg52@gmail.com>
2024-02-25 14:54:33 +0900

    refactor: 이벤트 핸들링을 새 파일 클래스로 분리

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 const { Client, Events, GatewayIntentBits, ButtonBuilder, ButtonStyle, ActionRowBuilder, ActivityType } = require('discord.js');
 const { loadEnvironmentVariables } = require('./library/functions.js');
 
@@ -5,9 +6,12 @@ loadEnvironmentVariables();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-const commandsHandler = (new require('./library/commands-handler.js'))();
+// TODO : commandsHandler -> CommandsHandler
+const commandsHandler = new (require('./library/commands-handler.js'))();
 const { commands } = commandsHandler.getCommands();
 client.commands = commands;
+
+const recruitHandler = new (require('./recruit-handler.js'))();
 
 client.login(process.env.DISCORD_TOKEN);
 
@@ -15,29 +19,33 @@ client.once(Events.ClientReady, async readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 
 	// 공지 채널이 비었으면 공지 메시지를 새로 보냄
-	const targetChannel = readyClient.channels.cache.find(channel => channel.name === '안내' && channel.guildId === process.env.DEPLOY_TARGET_GUILD_ID);
-	const messages = await targetChannel.messages.fetch({ limit: 1 });
+	const targetChannels = readyClient.channels.cache.filter(channel => channel.name === '안내');
 
-	if (!messages.some(item => item)) {
-		console.log('No messages in notice channel. making new one...');
+	for (const targetChannel of targetChannels.values()) {
+		console.log(`Checking notice channel... in guild :${targetChannel.guild.name} (${targetChannel.guild.id})`);
+		const messages = await targetChannel.messages.fetch({ limit: 1 });
 
-		const startRecruit = new ButtonBuilder()
-			.setCustomId('startRecruit')
-			.setLabel('가입 시작')
-			.setStyle(ButtonStyle.Success);
+		if (!messages.some(item => item)) {
+			console.log('No messages in notice channel. making new one...');
 
-		const startConv = new ButtonBuilder()
-			.setCustomId('startConv')
-			.setLabel('기타 문의')
-			.setStyle(ButtonStyle.Secondary);
+			const startRecruit = new ButtonBuilder()
+				.setCustomId('startRecruit')
+				.setLabel('가입 시작')
+				.setStyle(ButtonStyle.Success);
 
-		const row = new ActionRowBuilder()
-			.addComponents(startRecruit, startConv);
+			const startConv = new ButtonBuilder()
+				.setCustomId('startConv')
+				.setLabel('기타 문의')
+				.setStyle(ButtonStyle.Secondary);
 
-		targetChannel.send({
-			content: 'Nisuwaz 가입 절차를 시작하시려면 하단 \'가입 시작\' 버튼을, 다른 용무나 가입 관련 질문은 \'기타 문의\' 버튼을 통해 진행해 주세요',
-			components: [row],
-		});
+			const row = new ActionRowBuilder()
+				.addComponents(startRecruit, startConv);
+
+			targetChannel.send({
+				content: 'Nisuwaz 가입 절차를 시작하시려면 하단 \'가입 시작\' 버튼을, 다른 용무나 가입 관련 질문은 \'기타 문의\' 버튼을 통해 진행해 주세요',
+				components: [row],
+			});
+		}
 	}
 
 	// 활동 설정
@@ -54,4 +62,30 @@ client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
 	commandsHandler.executeCommands(interaction);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isButton()) return;
+
+	if (interaction.customId == 'startRecruit' || interaction.customId == 'startConv') {recruitHandler.startConvOrRecruit(interaction);}
+	else if (interaction.customId == 'startInputName') {recruitHandler.startInputName(interaction);}
+	else if (interaction.customId == 'agreeInformal') {recruitHandler.agreeInformal(interaction);}
+	else if (interaction.customId == 'agreePVP') {recruitHandler.agreePVP(interaction);}
+	else if (interaction.customId == 'registrationEnd') {recruitHandler.registrationEnd(interaction);}
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isStringSelectMenu()) return;
+
+	recruitHandler.confirmStringRoute(interaction);
+});
+
+client.on(Events.GuildMemberAdd, async member => {
+	member.send('안녕하세요! Nisuwaz 가입 안내 봇입니다. https://discord.com/channels/1200337363042312212/1200337363042312216/1200694031664033832 를 참고해 주세요!');
+});
+
+client.on(Events.MessageCreate, async message => {
+	if (message.content != '/조건숙지함') return;
+
+	recruitHandler.masterCondition(message);
 });
