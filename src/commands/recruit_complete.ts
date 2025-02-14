e2c558435aa35426cebe47ac2f5d1c1a7ce5d891
goemktg <samktg52@gmail.com>
2025-02-14 18:16:33 +0000

    feat: add ESLint configuration and update dependencies

--- src/commands/recruit_complete.ts
+++ src/commands/recruit_complete.ts
@@ -1,30 +1,38 @@
-import { SlashCommandBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } from 'discord.js';
-import { SlashCommand } from '../library/types';
+import {
+  SlashCommandBuilder,
+  ButtonBuilder,
+  ActionRowBuilder,
+  ButtonStyle,
+} from "discord.js";
+import { SlashCommand } from "../library/types";
 
 const FinishCounseling: SlashCommand = {
-	command: new SlashCommandBuilder()
-		.setName('상담완료')
-		.setDescription('상담을 완료시킵니다.'),
-	execute: interaction => {
-		const buttonOk = new ButtonBuilder()
-			.setCustomId('{"step":"finish-recruit","type":"yes"}')
-			.setLabel('예')
-			.setStyle(ButtonStyle.Success);
+  command: new SlashCommandBuilder()
+    .setName("상담완료")
+    .setDescription("상담을 완료시킵니다."),
+  execute: (interaction) => {
+    const buttonOk = new ButtonBuilder()
+      .setCustomId('{"step":"finish-recruit","type":"yes"}')
+      .setLabel("예")
+      .setStyle(ButtonStyle.Success);
 
-		const buttonNo = new ButtonBuilder()
-			.setCustomId('{"step":"finish-recruit","type":"no"}')
-			.setLabel('아니요')
-			.setStyle(ButtonStyle.Danger);
+    const buttonNo = new ButtonBuilder()
+      .setCustomId('{"step":"finish-recruit","type":"no"}')
+      .setLabel("아니요")
+      .setStyle(ButtonStyle.Danger);
 
-		const row = new ActionRowBuilder<ButtonBuilder>()
-			.addComponents(buttonOk, buttonNo);
+    const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      buttonOk,
+      buttonNo,
+    );
 
-		void interaction.reply({
-			content: '상담을 완료하면 상담 요청자가 해당 채널이 상담완료 카테고리로 옮겨지며 더 이상 대화를 나눌 수 없습니다. **__리크룻 중인 경우 상대방이 니수와 디스코드에 들어온 것__**을 반드시 확인 후 상담을 완료해 주십시오. 정말 상담을 완료하시겠습니까?',
-			components: [row],
-			ephemeral: true,
-		});
-	},
+    void interaction.reply({
+      content:
+        "상담을 완료하면 상담 요청자가 해당 채널이 상담완료 카테고리로 옮겨지며 더 이상 대화를 나눌 수 없습니다. **__리크룻 중인 경우 상대방이 니수와 디스코드에 들어온 것__**을 반드시 확인 후 상담을 완료해 주십시오. 정말 상담을 완료하시겠습니까?",
+      components: [row],
+      ephemeral: true,
+    });
+  },
 };
 
 export default FinishCounseling;
--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,101 @@
-import { Client, Events, GatewayIntentBits, ButtonBuilder, ButtonStyle, ActionRowBuilder, MessageCreateOptions } from 'discord.js';
-import { loadEnvironmentVariables, sendAnnouncementMsgs, setDefaultLogLevel, setDiscordPresence } from './library/functions';
-import { CommandsHandler } from './library/handlers/Commands';
-import { RecruitHandler } from './recruitHandler';
+import {
+  Client,
+  Events,
+  GatewayIntentBits,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+  MessageCreateOptions,
+} from "discord.js";
+import {
+  loadEnvironmentVariables,
+  sendAnnouncementMsgs,
+  setDefaultLogLevel,
+  setDiscordPresence,
+} from "./library/functions";
+import { CommandsHandler } from "./library/classes/CommandHandler";
+import { RecruitHandler } from "./recruitHandler";
 
 loadEnvironmentVariables();
 setDefaultLogLevel();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
 const commandsHandler = new CommandsHandler();
 const recruitHandler = new RecruitHandler();
 
 void client.login(process.env.DISCORD_TOKEN);
 
-client.once(Events.ClientReady, async readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 
-	client.commands = await commandsHandler.getCommandsFromDir();
+  void (async () => {
+    client.commands = await commandsHandler.getCommandsFromDir();
+  })();
 
-	const startRecruit = new ButtonBuilder()
-		.setCustomId('{"step":"create-channel","type":"recruit"}')
-		.setStyle(ButtonStyle.Success)
-		.setLabel('가입 시작');
+  const startRecruit = new ButtonBuilder()
+    .setCustomId('{"step":"create-channel","type":"recruit"}')
+    .setStyle(ButtonStyle.Success)
+    .setLabel("가입 시작");
 
-	const startDaehwa = new ButtonBuilder()
-		.setCustomId('{"step":"create-channel","type":"daehwa"}')
-		.setStyle(ButtonStyle.Secondary)
-		.setLabel('기타 문의');
+  const startDaehwa = new ButtonBuilder()
+    .setCustomId('{"step":"create-channel","type":"daehwa"}')
+    .setStyle(ButtonStyle.Secondary)
+    .setLabel("기타 문의");
 
-	const row = new ActionRowBuilder<ButtonBuilder>()
-		.addComponents(startRecruit, startDaehwa);
+  const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+    startRecruit,
+    startDaehwa,
+  );
 
-	const message = 'Nisuwaz / Corn Soup. Industrial 가입 절차를 시작하시려면 하단 \'가입 시작\' 버튼을, 다른 용무나 가입 관련 질문은 \'기타 문의\' 버튼을 통해 진행해 주세요 \n마지막 수정일 2024/05/27';
-	const channelMsg: MessageCreateOptions = { content: message, components: [row] };
+  const message =
+    "Nisuwaz / Corn Soup. Industrial 가입 절차를 시작하시려면 하단 '가입 시작' 버튼을, 다른 용무나 가입 관련 질문은 '기타 문의' 버튼을 통해 진행해 주세요 \n마지막 수정일 2024/05/27";
+  const channelMsg: MessageCreateOptions = {
+    content: message,
+    components: [row],
+  };
 
-	void sendAnnouncementMsgs(client, channelMsg);
+  void sendAnnouncementMsgs(client, channelMsg);
 
-	setDiscordPresence(readyClient.user, '리크루팅 관리 중');
+  setDiscordPresence(readyClient, "리크루팅 관리 중");
 });
 
-client.on(Events.InteractionCreate, interaction => {
-	if (!interaction.isChatInputCommand()) return;
+client.on(Events.InteractionCreate, (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-	void commandsHandler.executeCommand(interaction);
+  void commandsHandler.executeCommand(interaction);
 });
 
-client.on(Events.InteractionCreate, interaction => {
-	if (!interaction.isButton()) return;
+client.on(Events.InteractionCreate, (interaction) => {
+  if (!interaction.isButton()) return;
 
-	void recruitHandler.handleRecruitStep(interaction, interaction.customId);
+  void recruitHandler.handleRecruitStep(interaction, interaction.customId);
 });
 
-client.on(Events.InteractionCreate, interaction => {
-	if (!interaction.isStringSelectMenu()) return;
+client.on(Events.InteractionCreate, (interaction) => {
+  if (!interaction.isStringSelectMenu()) return;
 
-	void recruitHandler.handleRecruitStep(interaction, interaction.customId);
+  void recruitHandler.handleRecruitStep(interaction, interaction.customId);
 });
 
-client.on(Events.GuildMemberAdd, member => {
-	void member.send('안녕하세요! Nisuwaz 가입 안내 봇입니다. https://discord.com/channels/1200337363042312212/1200337363042312216/1200694031664033832 를 참고해 주세요!');
+client.on(Events.GuildMemberAdd, (member) => {
+  void member.send(
+    "안녕하세요! Nisuwaz 가입 안내 봇입니다. https://discord.com/channels/1200337363042312212/1200337363042312216/1200694031664033832 를 참고해 주세요!",
+  );
 });
 
-client.on(Events.MessageCreate, message => {
-	if (message.content != '/조건숙지함') return;
+client.on(Events.MessageCreate, (message) => {
+  if (message.content != "/조건숙지함") return;
 
-	void recruitHandler.handleRecruitStep(message, '{"step":"start-enter-main-char"}');
+  void recruitHandler.handleRecruitStep(
+    message,
+    '{"step":"start-enter-main-char"}',
+  );
 });
